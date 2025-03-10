ec9aad9023cd489966d1695dfbade65c869d888b
Affalximam <62225185+affalximam@users.noreply.github.com>
2025-03-10 14:21:17 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,8 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->environment('production')) {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-            config(['session.secure' => true]); // Paksa session HTTPS
+            URL::forceScheme('https');
         }
     }
 }
